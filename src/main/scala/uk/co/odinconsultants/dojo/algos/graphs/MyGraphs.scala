a9c924dad3619771edcfbe6a571c710c373a474c
Phillip Henry <PhillHenry@gmail.com>
2021-03-29 12:09:56 +0100

    Matrix multiplication.

--- src/main/scala/uk/co/odinconsultants/dojo/algos/graphs/MyGraphs.scala
+++ src/main/scala/uk/co/odinconsultants/dojo/algos/graphs/MyGraphs.scala
@@ -4,10 +4,35 @@ object MyGraphs {
 
   type Matrix = Array[Array[Double]]
 
-  def emptyMatrix(nRows: Int, nCols: Int): Matrix = {
+  def multiply(xMatrix: Matrix, yMatrix: Matrix): Matrix = {
+    val xCols = xMatrix(0).length
+    val xRows = xMatrix.length
+    val yCols = yMatrix(0).length
+    val yRows = yMatrix.length
+
+    assert(xCols == yRows)
+
+    val toReturn = matrix(xRows, yCols, 0)
+    for {
+      i <- 0 until xRows
+      j <- 0 until yCols
+    } yield {
+      val xs = xMatrix(i)
+      val ys = for {
+        k <- 0 until yRows
+      } yield {
+        yMatrix(k)(j)
+      }
+      val multiplied = xs.zip(ys).map { case (x, y) => x * y }
+      toReturn(i)(j) = multiplied.sum
+    }
+    toReturn
+  }
+
+  def matrix(nRows: Int, nCols: Int, initial: Double = 0d): Matrix = {
     val xs = for {
       _ <- 1 to nRows
-    } yield Array.fill(nCols)(0d)
+    } yield Array.fill(nCols)(initial)
     xs.toArray
   }
 
--- src/test/scala/uk/co/odinconsultants/dojo/algos/graphs/MyGraphsSpec.scala
+++ src/test/scala/uk/co/odinconsultants/dojo/algos/graphs/MyGraphsSpec.scala
@@ -1,31 +1,51 @@
 package uk.co.odinconsultants.dojo.algos.graphs
 
+import org.scalatest.Assertion
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import uk.co.odinconsultants.dojo.algos.graphs.MyGraphs.emptyMatrix
+import uk.co.odinconsultants.dojo.algos.graphs.MyGraphs._
 
 class MyGraphsSpec extends AnyWordSpec with Matchers {
+
+  "Matrix multiplication" should {
+    val xRows = 7
+    val yRows = 11
+    val yCols = 13
+    val xMatrix = matrix(xRows, yRows)
+    val yMatrix = matrix(yRows, yCols)
+    "yield the correct shape" in {
+      val m = multiply(xMatrix, yMatrix)
+      checkMatrixShape(xRows, yCols, m)
+    }
+  }
 
   "Empty matrix" should {
     val nRows = 7
     val nCols = 11
-    val m = emptyMatrix(nRows, nCols)
+    val m = matrix(nRows, nCols)
     s"have size $nRows x $nCols" in {
-      m should have length nRows
-      for {
-        row <- m
-      } yield {
-        row should have length nCols
-      }
+      checkMatrixShape(nRows, nCols, m)
     }
     "have all elements equal to 0" in {
-      for {
-        row <- m
-        x <- row
-      } yield {
-        x shouldBe 0d
-      }
+      checkMatrixContainsOnlyZeros(m)
     }
   }
 
+  private def checkMatrixShape(nRows: Int, nCols: Int, m: Matrix): Unit = {
+    m should have length nRows
+    for {
+      row <- m
+    } yield {
+      row should have length nCols
+    }
+  }
+
+  private def checkMatrixContainsOnlyZeros(m: Matrix): Unit = {
+    for {
+      row <- m
+      x <- row
+    } yield {
+      x shouldBe 0d
+    }
+  }
 }
