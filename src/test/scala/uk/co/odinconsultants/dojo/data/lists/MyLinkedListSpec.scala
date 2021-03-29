ad2e2398ad22fafaca45e2cd6b9d092aec59f802
Phillip Henry <PhillHenry@gmail.com>
2021-03-29 12:32:08 +0100

    Linked list - more tests.

--- src/test/scala/uk/co/odinconsultants/dojo/data/lists/MyLinkedListSpec.scala
+++ src/test/scala/uk/co/odinconsultants/dojo/data/lists/MyLinkedListSpec.scala
@@ -4,20 +4,29 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import uk.co.odinconsultants.dojo.data.lists.MyLinkedList.makeMyLinkedList
 
+import scala.collection.mutable.ArrayBuffer
+
 class MyLinkedListSpec extends AnyWordSpec with Matchers {
 
-  val xs = Array(1,2,3,5,7,11)
+  val expected = Array(1,2,3,5,7,11)
 
-  s"Linked list of ${xs.mkString(", ")}" should {
-    s"have ${xs.length} elements" in {
-      val linked = makeMyLinkedList(xs)
+  s"Linked list of ${expected.mkString(", ")}" should {
+    s"have ${expected.length} elements" in {
+      val linked = makeMyLinkedList(expected)
+      val actual = new ArrayBuffer[Int]()
+      actual.append(linked.t)
       var i = 1
       var next = linked.next
       while (next.isDefined) {
         i = i + 1
-        next = next.get.next
+        val element = next.get
+        actual.append(element.t)
+        next = element.next
       }
-      i shouldBe xs.length
+      i shouldBe expected.length
+      withClue(linked) {
+        actual.toArray shouldBe expected
+      }
     }
   }
 
