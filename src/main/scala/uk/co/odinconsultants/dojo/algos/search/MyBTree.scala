c4b1467308bf06c06a3e53421daf8991ab6c4806
Phillip Henry <PhillHenry@gmail.com>
2021-05-23 20:49:25 +0100

    First draft of a B-tree.

---
+++ src/main/scala/uk/co/odinconsultants/dojo/algos/search/MyBTree.scala
@@ -0,0 +1,20 @@
+package uk.co.odinconsultants.dojo.algos.search
+
+case class Node[K: Ordering, V](children: Seq[(K, Node[K, V])]) {
+
+  val sorted = children.sortBy(_._1)
+
+  def lessThan(x: K, y: K): Boolean = Ordering[K].compare(x, y) <= 0
+
+  def findIndexOf(k: K): Int =
+    sorted.zipWithIndex
+      .find { case ((k1, _), _) => lessThan(k, k1) }
+      .map { case ((_, _), i) => i }
+      .getOrElse(sorted.size)
+
+
+}
+
+class MyBTree[K: Ordered, V](m: Int, root: Node[K, V]) {
+
+}
---
+++ src/test/scala/uk/co/odinconsultants/dojo/algos/search/MyBTreeSpec.scala
@@ -0,0 +1,45 @@
+package uk.co.odinconsultants.dojo.algos.search
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class MyBTreeSpec extends AnyWordSpec with Matchers {
+
+  type K = String
+  type V = Int
+  type KV = (K, Node[K, V])
+
+  val emptyChildren = Seq.empty[KV]
+  val emptyNode = Node(emptyChildren)
+
+  trait PopulatedNode {
+    val keyA = "a"
+    val keyB = "b"
+    val keyC = "c"
+    val keyD = "d"
+    val keyE = "e"
+    val keyF = "f"
+    val keyG = "g"
+    val a: KV = (keyA, emptyNode)
+    val b: KV = (keyB, emptyNode)
+    val c: KV = (keyC, emptyNode)
+    val d: KV = (keyD, emptyNode)
+    val e: KV = (keyE, emptyNode)
+    val f: KV = (keyF, emptyNode)
+    val g: KV = (keyG, emptyNode)
+  }
+
+  "Searching" should {
+    "find the right index" in new PopulatedNode {
+      val toTest = Node(Seq(a, c, e))
+      toTest.findIndexOf(keyA) shouldBe 0
+      toTest.findIndexOf(keyC) shouldBe 1
+      toTest.findIndexOf(keyE) shouldBe 2
+    }
+    "find the gap" in new PopulatedNode {
+      val toTest = Node(Seq(b, d, f))
+      toTest.findIndexOf(keyA) shouldBe 0
+      toTest.findIndexOf(keyG) shouldBe 3
+    }
+  }
+}
